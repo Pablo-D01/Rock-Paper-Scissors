81e30a3abc1b36b388c440456db4fb0523e01711
Paweł D <you@example.com>
2022-12-14 17:54:33 +0100

    added buttons and one round game function

--- script.js
+++ script.js
@@ -1,28 +1,21 @@
-// 1 - Rock
-// 2 - Paper
-// 3 - Scissors
-function getComputerChoice(){
-    choice = Math.floor(Math.random() * 3 + 1)
-    if (choice === 1) {
-        return "Rock"
-    }
-    else if ( choice === 2) {
-        return "Paper"
-    }
-    else {
-        return "Scissors"
-    }
-}
-
 
 let playerScore = 0 ;
 let computerScore = 0;
+let roundCounter = 0;
 
+
+
+
+
+function getComputerChoice(){
+    choice = Math.floor(Math.random() * 3 + 1)
+    if (choice === 1) { return "Rock" }
+    else if ( choice === 2) {  return "Paper" }
+    else { return "Scissors" }
+}
 
 function playRound(playerSelection, computerSelection) {
-    // console.log('PC:', computerSelection)
-    // console.log('PLAYER:', playerSelection)
-
+    
     if (playerSelection === "Rock" && computerSelection === "Rock") { return undefined }
     else if (playerSelection ==="Rock" && computerSelection === "Paper" ) {return computerScore = computerScore +1 }
     else if (playerSelection ==="Rock" && computerSelection === "Scissors" ) {return playerScore = playerScore +1 }
@@ -37,26 +30,52 @@ function playRound(playerSelection, computerSelection) {
     
   }
    
-  
-//   const playerSelection = "Rock";
-//   const computerSelection = getComputerChoice();
-//   console.log(playRound(playerSelection, computerSelection));
 
-  function game(playerSelection){
+
+
+
+  function gameAuto(playerSelection){
     computerScore = 0 ;
     playerScore = 0 ;
-    for (let i = 0 ; i < 15; i++ ){
+    for (let i = 0 ; i < 15; i++ ) {
         let computerSelection = getComputerChoice();
         playRound(playerSelection, computerSelection);
         if ( playerScore === 5 ) { alert("Player wins !"); break }
         else if (computerScore === 5 ) {alert("Computer wins !"); break}
-        x = i
-        
-    
-
+           
     }
     console.log("pc: ", computerScore)
     console.log("player: ", playerScore)
-    console.log('Rounds played: ', x)
+    console.log('Rounds played: ', roundCounter)
   }
 
+
+
+
+function showResults(){
+    
+    alert("Rounds played: "+ roundCounter);
+    computerScore = 0 ;
+    playerScore = 0 ;
+    roundCounter = 0 ;
+    
+}
+
+
+function game(playerSelection){
+    let computerSelection = getComputerChoice();
+    playRound(playerSelection, computerSelection);
+    // console.log("pc: ", computerScore)
+    // console.log("player: ", playerScore)
+    roundCounter = roundCounter +1 ;
+
+    if ( playerScore === 5 ) { 
+        alert("Player wins !");
+        showResults();  
+}
+    else if (computerScore === 5 ) {
+        alert("Computer wins !");
+        showResults();
+    }
+
+}
