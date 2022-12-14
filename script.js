1e913924d809a693bac4302b0bad88f08ceeb523
Paweł D <you@example.com>
2022-12-14 17:29:35 +0100

    Added loop function game()

--- script.js
+++ script.js
@@ -14,25 +14,49 @@ function getComputerChoice(){
     }
 }
 
+
+let playerScore = 0 ;
+let computerScore = 0;
+
+
 function playRound(playerSelection, computerSelection) {
-    console.log('PC:', computerSelection)
-    console.log('PLAYER:', playerSelection)
+    // console.log('PC:', computerSelection)
+    // console.log('PLAYER:', playerSelection)
 
-    if (playerSelection === "Rock" && computerSelection === "Rock") { return "TIE!" }
-    else if (playerSelection ==="Rock" && computerSelection === "Paper" ) { return "Computer Wins"  }
-    else if (playerSelection ==="Rock" && computerSelection === "Scissors" ) {return "Player wins"  }
+    if (playerSelection === "Rock" && computerSelection === "Rock") { return undefined }
+    else if (playerSelection ==="Rock" && computerSelection === "Paper" ) {return computerScore = computerScore +1 }
+    else if (playerSelection ==="Rock" && computerSelection === "Scissors" ) {return playerScore = playerScore +1 }
 
-    else if (playerSelection === "Paper" && computerSelection ==="Rock") {return "Player wins"  }
-    else if (playerSelection === "Paper" && computerSelection ==="Paper") {return "TIE!"  }
-    else if (playerSelection === "Paper" && computerSelection ==="Scissors") {return "Computer Wins"  }
+    else if (playerSelection === "Paper" && computerSelection ==="Rock") {return playerScore = playerScore +1  }
+    else if (playerSelection === "Paper" && computerSelection ==="Paper") { return undefined  }
+    else if (playerSelection === "Paper" && computerSelection ==="Scissors") {return computerScore = computerScore +1  }
 
-    else if (playerSelection === "Scissors" && computerSelection ==="Rock") {return "Computer Wins" }
-    else if (playerSelection === "Scissors" && computerSelection ==="Paper") {return "Player wins"}
-    else if (playerSelection === "Scissors" && computerSelection ==="Scissors") {return "TIE!"}
+    else if (playerSelection === "Scissors" && computerSelection ==="Rock") {return computerScore = computerScore +1 }
+    else if (playerSelection === "Scissors" && computerSelection ==="Paper") {return playerScore = playerScore +1}
+    else if (playerSelection === "Scissors" && computerSelection ==="Scissors") { return undefined}
     
   }
    
-  const playerSelection = "Rock";
-  const computerSelection = getComputerChoice();
-  console.log(playRound(playerSelection, computerSelection));
+  
+//   const playerSelection = "Rock";
+//   const computerSelection = getComputerChoice();
+//   console.log(playRound(playerSelection, computerSelection));
+
+  function game(playerSelection){
+    computerScore = 0 ;
+    playerScore = 0 ;
+    for (let i = 0 ; i < 15; i++ ){
+        let computerSelection = getComputerChoice();
+        playRound(playerSelection, computerSelection);
+        if ( playerScore === 5 ) { alert("Player wins !"); break }
+        else if (computerScore === 5 ) {alert("Computer wins !"); break}
+        x = i
+        
+    
+
+    }
+    console.log("pc: ", computerScore)
+    console.log("player: ", playerScore)
+    console.log('Rounds played: ', x)
+  }
 
